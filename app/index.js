096b7f3d9980c4a1435067411650bc7d1cd4219b
frux <frux@yandex-team.ru>
2019-04-16 21:03:26 +0500

    fix indent

--- app/index.js
+++ app/index.js
@@ -5,28 +5,28 @@ const GENERATORS = {
 };
 
 module.exports = class extends Generator {
-    constructor(args, opts) {
-        super(args, opts);
-    }
+	constructor(args, opts) {
+		super(args, opts);
+	}
 
-    async prompting() {
-        this.answers = await this.prompt([
-            {
-                type: "checkbox",
-                name: "generators",
-                message: "Pick what you need",
-                choices: Object.keys(GENERATORS).map(generatorName => ({
-                    name: GENERATORS[generatorName],
-                    value: generatorName
-                })),
-                default: Object.keys(GENERATORS)
-            }
-        ]);
-    }
+	async prompting() {
+		this.answers = await this.prompt([
+			{
+				type: "checkbox",
+				name: "generators",
+				message: "Pick what you need",
+				choices: Object.keys(GENERATORS).map(generatorName => ({
+					name: GENERATORS[generatorName],
+					value: generatorName
+				})),
+				default: Object.keys(GENERATORS)
+			}
+		]);
+	}
 
-    writing() {
-        this.answers.generators.forEach(generator => {
-            this.composeWith(`ziggurat:${generator}`)
-        });
-    }
+	writing() {
+		this.answers.generators.forEach(generator => {
+			this.composeWith(`ziggurat:${generator}`)
+		});
+	}
 };
